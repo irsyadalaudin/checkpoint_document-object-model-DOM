b887c0de472968c024e9a113e51a9ededdcf4198
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-04-01 15:46:33 +0200

    adding .gitignore and function addToCart() in index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+/** */
 const minusButton = document.querySelectorAll('.minus');
 const quantityField = document.querySelectorAll('.quantity');
 const plusButton = document.querySelectorAll('.plus');
 
 for (let i = 0; i < plusButton.length; i++) {
     plusButton[i].addEventListener('click', function() {
-        let currentQuantity = parseInt(quantityField[i].innerHTML);     // Fungsi ini mengambil nilai dari quantityField[i].innerHTML yang telah dikonversi menjadi integer menggunakan parseInt()  dan disimpan pada variabel currentQuantity
-        quantityField[i].innerHTML = currentQuantity + 1;               // Kemudian nilai currentQuantity ditambah 1 dan disimpan kembali pada quantityField[i].innerHTML
+        let currentQuantity = parseInt(quantityField[i].innerHTML);     	 // Fungsi ini mengambil nilai dari quantityField[i].innerHTML yang telah dikonversi menjadi integer menggunakan parseInt()  dan disimpan pada variabel currentQuantity
+        quantityField[i].innerHTML = currentQuantity + 1;               	 // Kemudian nilai currentQuantity ditambah 1 dan disimpan kembali pada quantityField[i].innerHTML
     });
 }
 
@@ -19,30 +20,17 @@ for (let i = 0; i < minusButton.length; i++) {
 }
 
 
-// const removeItem = document.getElementById('remove');
-// const item = removeItem.parentElement.parentElement;
 
-// removeItem.addEventListener('click', function() {
-//     item.remove();
-// });
-
+/** */
 const removeItem = document.getElementById('remove');
 removeItem.addEventListener('click', function() {
     const item = this.parentElement.parentElement;
     item.remove();
 });
-console.log(removeItem)
 
 
-// const likeButton = document.getElementById('like-button');
-// likeButton.addEventListener('click', function() {
-//     if(likeButton.style.fill === 'red') {
-//         likeButton.style.fill = 'white';
-//     } else {
-//         likeButton.style.fill = 'red'
-//     }
-// });
 
+/** */
 const likeButton = document.getElementById('like-button');
 likeButton.addEventListener('click', function() {
     if(likeButton.getAttribute('fill') === 'red') {
@@ -53,8 +41,45 @@ likeButton.addEventListener('click', function() {
 });
 
 
-// const addToCart = document.getElementsById('adding-card');    // TOMBOL, YANG KETIKA DI-KLIK AKAN MEMASUKKAN BARANG KE DALAM CART
-// const cart = document.getElementById('cart')                  // CART NYA, YANG DIMANA TEMPAT UNTUK MENAMPUNG BARANG
-// addToCart.addEventListener('click', function() {
-//     const newSepatu = document.createElement('div')
-// });
+
+/** */
+function addToCart() {
+	// Get all the "ADD TO CART" buttons
+	const addToCartButtons = document.querySelectorAll('#adding-cart');
+
+	// Add an event listener to each button
+	addToCartButtons.forEach(function(button) {
+		button.addEventListener('click', function() {
+			// Get the relevant information from the "shoes" div
+			const shoes = button.parentElement.parentElement;
+			const shoesPhotos = shoes.querySelector('.shoes-photos').src;      // '.src' digunakan untuk mengambil nilai dari atribut tertentu pada elemen HTML yang dipilih.  // BISA JUGA MENGGUNAKAN getAttribute(), kita dapat mengambil nilai dari atribut tertentu pada suatu elemen HTML.
+			const shoesName = shoes.querySelector('h3').textContent;
+			const shoesPrice = shoes.querySelector('.price').textContent;
+
+			// Create a new "shoes-cart" div with the retrieved information
+			const shoesIntoCart = document.createElement('div');
+			shoesIntoCart.classList.add('shoes-cart');
+			shoesIntoCart.innerHTML = `
+				<img class="shoes-photos" src="${shoesPhotos}" alt="">
+				<h3>${shoesName}</h3>
+				<h2 class="price">${shoesPrice}</h2>
+				<div class="result">
+				<button id="remove">remove</button>
+				<svg id="like-button" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / Heart_01"> <path id="Vector" d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+					<div class="cart-total">
+						<button class="minus">-</button>
+						<span class="quantity">1</span>
+						<button class="plus">+</button>
+					</div>
+				</div>
+			`;
+		
+			// Append the new "shoes-cart" div to the "CART" section
+			const cartOrder = document.querySelector('#cart');
+			cartOrder.parentElement.insertBefore(shoesIntoCart, cartOrder.nextSibling);
+		});
+	});
+}
+
+// Call the function
+addToCart();
