7f74a4c08e422bcb4cb1ff23e70f34ef9fca748f
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-04-02 08:24:32 +0200

    reparing all and changing name js to main.js and make main.js in function addToCart()

--- main.js
+++ main.js
@@ -1,48 +1,4 @@
 /** */
-const minusButton = document.querySelectorAll('.minus');
-const quantityField = document.querySelectorAll('.quantity');
-const plusButton = document.querySelectorAll('.plus');
-
-for (let i = 0; i < plusButton.length; i++) {
-    plusButton[i].addEventListener('click', function() {
-        let currentQuantity = parseInt(quantityField[i].innerHTML);     	 // Fungsi ini mengambil nilai dari quantityField[i].innerHTML yang telah dikonversi menjadi integer menggunakan parseInt()  dan disimpan pada variabel currentQuantity
-        quantityField[i].innerHTML = currentQuantity + 1;               	 // Kemudian nilai currentQuantity ditambah 1 dan disimpan kembali pada quantityField[i].innerHTML
-    });
-}
-
-for (let i = 0; i < minusButton.length; i++) {
-    minusButton[i].addEventListener('click', function() {
-        let currentQuantity = parseInt(quantityField[i].innerHTML);
-        if (currentQuantity > 0) {
-            quantityField[i].innerHTML = currentQuantity - 1;
-      }
-    });
-}
-
-
-
-/** */
-const removeItem = document.getElementById('remove');
-removeItem.addEventListener('click', function() {
-    const item = this.parentElement.parentElement;
-    item.remove();
-});
-
-
-
-/** */
-const likeButton = document.getElementById('like-button');
-likeButton.addEventListener('click', function() {
-    if(likeButton.getAttribute('fill') === 'red') {
-        likeButton.setAttribute('fill', 'white');
-    } else {
-        likeButton.setAttribute('fill', 'red')
-    }
-});
-
-
-
-/** */
 function addToCart() {
 	const addToCartButtons = document.querySelectorAll('#adding-cart');
 
@@ -70,10 +26,44 @@ function addToCart() {
 					</div>
 				</div>
 			`;
-		
-			// Append the new "shoes-cart" div to the "CART" section
+
 			const cartOrder = document.querySelector('#cart');
 			cartOrder.parentElement.insertBefore(shoesIntoCart, cartOrder.nextSibling);
+			
+			const minusButton = document.querySelectorAll('.minus');
+			const quantityField = document.querySelectorAll('.quantity');
+			const plusButton = document.querySelectorAll('.plus');
+
+			for (let i = 0; i < plusButton.length; i++) {
+				plusButton[i].addEventListener('click', function() {
+					let currentQuantity = parseInt(quantityField[i].innerHTML);
+					quantityField[i].innerHTML = currentQuantity + 1;
+				});
+			}
+
+			for (let i = 0; i < minusButton.length; i++) {
+				minusButton[i].addEventListener('click', function() {
+					let currentQuantity = parseInt(quantityField[i].innerHTML);
+					if (currentQuantity > 0) {
+						quantityField[i].innerHTML = currentQuantity - 1;
+				}
+				});
+			}
+
+			const removeItem = shoesIntoCart.querySelector('#remove');
+            removeItem.addEventListener('click', function() {
+                shoesIntoCart.remove();
+            });
+			
+			const likeButton = document.getElementById('like-button');
+			likeButton.addEventListener('click', function() {
+				if(likeButton.getAttribute('fill') === 'red') {
+					likeButton.setAttribute('fill', 'white');
+				} else {
+					likeButton.setAttribute('fill', 'red')
+				}
+			});
+
 		});
 	});
 }
